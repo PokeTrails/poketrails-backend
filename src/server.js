3d9645277a155f7c9a00bdf60fa04538c66e886a
Suraj-Razor <14635@coderacademy.edu.au>
2024-07-22 10:58:37 +1000

    feat: added generic error handling

--- src/server.js
+++ src/server.js
@@ -9,6 +9,20 @@ const app = express();
 // Logger middleware
 app.use(logger);
 
+app.get("/", (req, res, next) => {
+    res.status(200).json({
+        msg: "Hello World"
+    });
+});
+
+//Generic error handling for any error that happens on the server
+app.use((error, req, res, next) => {
+    res.status(500).json({
+        msg: "Error occured in the server",
+        error: error.message
+    });
+});
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
