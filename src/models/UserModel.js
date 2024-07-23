764d7c8b6acd7e37af4c60badc6277c72f067ae4
Taliesin <taliesinstudy@gmail.com>
2024-07-24 01:30:31 +1000

    feat! auth for seeding created

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = mongoose.Schema({
 
@@ -21,6 +22,26 @@ const userSchema = mongoose.Schema({
     //     partyBuff:{},
     // }
 })
+
+userSchema.pre(
+    "save",
+    async function (next) {
+        const user = this;
+        console.log("Pre-save hook running");
+        
+        if (!user.isModified("password")){
+            return;
+        }
+        console.log("Raw password is: " + this.password);
+
+        const hash = await bcrypt.hash(this.password, 10);
+        console.log("Hashed and encrypted and salted password is: " + hash);
+
+        this.password = hash;
+
+        next();
+    }
+)
 
 const UserModel = mongoose.model("User", userSchema)
 
