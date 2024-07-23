2103c81516c305d5cef4fbbdf9f57d92f1ffd745
Taliesin <taliesinstudy@gmail.com>
2024-07-23 20:02:39 +1000

    feat! user seed skeleton created

---
+++ src/utils/seed.js
@@ -0,0 +1,39 @@
+const { connectDB } = require("../config/database");
+const { UserModel } = require("../models/UserModel");
+
+
+
+
+async function seedUsers(){
+    let userData = [
+        {
+            username: "pokeking",
+            firstName: "poke",
+            lastName: "king",
+            email: "pickachu12@gmail.com",
+            password: "password",
+            secretQuestion: "3+3",
+            secretAnswer: "6"
+        }
+    ];
+
+    let result = await UserModel.insertMany(userData);
+    console.log(result)
+    return result;
+}
+
+
+async function seed(){
+
+    await connectDB();
+    await clearDB();
+
+    let newUsers = await seedUsers();
+    console.log("The new users are: ")
+    console.log(newUsers)
+
+    await closeDB();
+
+}
+
+seed();
