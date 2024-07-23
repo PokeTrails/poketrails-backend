b1a702f8dcfdf5de3178a42a02db5219c3835b51
Taliesin <taliesinstudy@gmail.com>
2024-07-23 20:40:00 +1000

    feat! auth functions added

---
+++ src/utils/authHelper.js
@@ -0,0 +1,46 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+require("dotenv").config();
+
+async function comparePasswords(plaintextPassword, encryptedPassword) {
+    let doesPasswordMatch = false;
+
+    doesPasswordMatch = await bcrypt.compare(plaintextPassword, encryptedPassword);
+    
+    return doesPasswordMatch;
+}
+
+
+function createJWT(userID){
+    let newJwt = jwt.sign(
+        // Payloard of data
+        {id: userID},
+        // Secret key for JWT signature
+        process.env.JWT_KEY,
+
+        // Options for JWT expiry
+        {
+            expiresIn: "7d"
+        }
+    )
+
+    return newJwt;
+}
+
+
+function validateJWT(jwtToValidate){
+    let isJwtValid = false;
+
+    jwt.verify(jwtToValidate, process.env.JWT_KEY, (error, decodedJwt) => {
+        if (error){
+            throw new Error("user JWT is not valid!");
+        }
+        console.log("Decoded JWT:")
+        console.log(decodedJwt);
+
+        isJwtValid = true;
+    });
+    
+    return isJwtValid;
+}
