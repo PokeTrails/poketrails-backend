1cf7bd12a15f730d0160b6ea6621c16bd3fb40dd
Taliesin <taliesinstudy@gmail.com>
2024-07-25 16:42:46 +1000

    feat! user get routes

--- src/routes/UserRouter.js
+++ src/routes/UserRouter.js
@@ -1,10 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const { UserModel } = require("../models/UserModel")
 
+
+// Route to find all active users
 router.get("/", async (request, response, next) => {
+
+    let result = await UserModel.find({}).exec();
+
     response.json({
-        message:"user router test"
-    })
-})
+        message:"user router test",
+        result: result
+    });
+});
+
+// Route to find user with matching ID
+router.get("/:id", async (request, response, next) => {
+
+    let result = await UserModel.findById(request.params.id).exec();
+
+    response.json({
+        message:"user router test",
+        result: result
+    });
+});
+
+
 
 module.exports = router;
