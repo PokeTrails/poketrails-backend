94506dcdf4cd9be9da748cc305fc089b99ffe2f2
Taliesin <taliesinstudy@gmail.com>
2024-07-26 14:03:45 +1000

    fix! Userschema updated

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -4,17 +4,16 @@ const bcrypt = require("bcryptjs");
 const userSchema = mongoose.Schema({
 
     username:{type: String, required: true, unique: true},
-    firstName:{type: String, required: true},
-    lastName:{type: String, required: true},
     email:{type: String, required: true, unique: true},
     password:{type: String, required: true, unique: false},
-    secretQuestion:{ type: String, required: true},
-    secretAnswer:{type: String, required: true},
+    secretQuestion:{ type: String},
+    secretAnswer:{type: String},
     userExperience:{type: Number, default: 0, required: true},
     playerLevel:{type: Number, default: 1, required: true},
     isFirstLogin:{type: Boolean, default: true},
     balance:{type: Number, default: 0, required: true},   
-    eggVoucher:{type: Number, default: 0, required: true}
+    eggVoucher:{type: Number, default: 0, required: true},
+    trainerSprite:{type: String, default: "Male"}
     // party:{
     //     partyID:{},
     //     slots:{type:[partySlotSchema]}, Will import seperate schema for the party slots
