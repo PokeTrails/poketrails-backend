b926995d7184a2a5d27696ce564ddbd5547cca63
Suraj-Razor <14635@coderacademy.edu.au>
2024-07-28 17:50:16 +1000

    feat: Added party model

---
+++ src/models/PartyModel.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const partySchema = mongoose.Schema({
+    slots: [{ type: mongoose.Schema.Types.ObjectId, ref: "Pokemon" }],
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    buffs: { type: [String] } // Use String instead of string
+});
+
+const PartyModel = mongoose.model("Party", partySchema);
+
+module.exports = PartyModel;
