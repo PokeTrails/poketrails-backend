57f6d70bcc5ab6833570b84d9456f36ac7a40d87
Suraj-Razor <14635@coderacademy.edu.au>
2024-07-28 21:54:14 +1000

    fix: fixed the response on party to respond with the necessary data only

--- src/controllers/PartyController.js
+++ src/controllers/PartyController.js
@@ -2,8 +2,11 @@ const express = require("express");
 const Party = require("../models/PartyModel");
 
 const getAllParties = async (req, res) => {
-    const parties = await Party.find({ user: req.userId });
-    res.status(200).json(parties);
+    const parties = await Party.findOne({ user: req.userId });
+    res.status(200).json({
+        slots: parties.slots,
+        buffs: parties.buffs
+    });
 };
 
 module.exports = { getAllParties };
