adac1baea07b548377fab94aa7f18147bcde4e74
Suraj-Razor <14635@coderacademy.edu.au>
2024-08-03 22:49:50 +1000

    fix: removed console logs

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -13,45 +13,40 @@ const userSchema = mongoose.Schema({
     balance: { type: Number, default: 0, required: true },
     eggVoucher: { type: Number, default: 0, required: true },
     trainerSprite: { type: String, required: true },
-    happinesMulti: { type: Number, default: 1, required: true},
-    shinyMulti: { type: Number, default: 1, required: true},
-    trailMulti: { type: Number, default: 1, required: true},
-    moneyMulti: { type: Number, default: 1, required: true},
-    shopItems: [{
-        itemName: { type: String, required: true },
-        price: { type: Number, required: true },
-        level: { type: Number, required: true },
-        owned: { type: Boolean, default: false, required: true}
-    }]
+    happinesMulti: { type: Number, default: 1, required: true },
+    shinyMulti: { type: Number, default: 1, required: true },
+    trailMulti: { type: Number, default: 1, required: true },
+    moneyMulti: { type: Number, default: 1, required: true },
+    shopItems: [
+        {
+            itemName: { type: String, required: true },
+            price: { type: Number, required: true },
+            level: { type: Number, required: true },
+            owned: { type: Boolean, default: false, required: true }
+        }
+    ]
 });
 
 const defaultShopItems = [
-    { itemName: 'expShare', price: 600, level: 1 },
-    { itemName: 'shinyCharm', price: 600, level: 1 },
-    { itemName: 'runningShoes', price: 600, level: 1 },
-    { itemName: 'amuletCoin', price: 600, level: 1 }
+    { itemName: "expShare", price: 600, level: 1 },
+    { itemName: "shinyCharm", price: 600, level: 1 },
+    { itemName: "runningShoes", price: 600, level: 1 },
+    { itemName: "amuletCoin", price: 600, level: 1 }
 ];
 
 userSchema.pre("save", async function (next) {
     const user = this;
-    console.log("Pre-save hook running");
 
     if (!user.isModified("password")) {
         return;
     }
-    console.log("Raw password is: " + this.password);
 
     const hash = await bcrypt.hash(this.password, 10);
-    console.log("Hashed and encrypted and salted password is: " + hash);
-
     this.password = hash;
 
     if (this.shopItems && this.shopItems.length === 0) {
         this.shopItems = defaultShopItems;
     }
-
-    console.log( "New user has these items:" + this.shopItems);
-
     next();
 });
 
