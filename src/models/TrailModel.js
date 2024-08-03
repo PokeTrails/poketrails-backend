5e4bb02999961c7713d1ed148a939a6eca85dd4d
Taliesin <taliesinstudy@gmail.com>
2024-08-03 12:11:37 +1000

    feat! TrailModel Created

---
+++ src/models/TrailModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+
+const pokemonTypes =  [
+    "Normal",
+    "Fire",
+    "Water",
+    "Electric",
+    "Grass",
+    "Ice",
+    "Fighting",
+    "Poison",
+    "Ground",
+    "Flying",
+    "Psychic",
+    "Bug",
+    "Rock",
+    "Ghost",
+    "Dragon",
+    "Dark",
+    "Steel",
+    "Fairy"
+];
+
+const TrailSchema = mongoose.Schema({
+    title: {type: String, required: true},
+    traillog: [{type: mongoose.Schema.Types.ObjectId, ref: 'TrailLogModel', required: true}],
+    buffedTypes: {type: String, required: true},
+    enum: {pokemonTypes}
+})
+
+const TrailModel = mongoose.model("Trail", TrailSchema);
+
+module.exports = { TrailModel };
