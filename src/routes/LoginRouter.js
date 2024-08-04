df8d21424b03cb89518e62a2da7bf1976487e906
Suraj-Razor <14635@coderacademy.edu.au>
2024-08-04 21:52:31 +1000

    feat: happiness/shiny/money are set to increase based off multis on user profile

--- src/routes/LoginRouter.js
+++ src/routes/LoginRouter.js
@@ -1,48 +1,41 @@
 const express = require("express");
 const router = express.Router();
 
-const { comparePasswords, createJWT } = require("../utils/authHelper")
+const { comparePasswords, createJWT } = require("../utils/authHelper");
 const { UserModel } = require("../models/UserModel");
 
-
-
 router.post("/", async (request, response, next) => {
-	let newJwt = "";
+    let newJwt = "";
 
-	if (!request.body.password || !request.body.username){
-		return next(new Error("Missing login details in login request."));
-	}
-
-	try {
-		// Find user by username in DB
-	let foundUser = await UserModel.findOne({username: request.body.username}).exec();
-
-	console.log(request.body, foundUser);
-	let isPasswordCorrect = false;
-
-	// Compare request.body.password to foundUser.password using the compare function
-    if (foundUser){ 
-	    isPasswordCorrect = await comparePasswords(request.body.password, foundUser.password);
-    }
-    else{
-        return next(new Error("Incorrect username."));
+    if (!request.body.password || !request.body.username) {
+        return next(new Error("Missing login details in login request."));
     }
 
-	// Create a JWT based on foundUser._id 
-	if (isPasswordCorrect){
+    try {
+        // Find user by username in DB
+        let foundUser = await UserModel.findOne({ username: request.body.username }).exec();
+        let isPasswordCorrect = false;
 
-		newJwt = createJWT(foundUser._id);
+        // Compare request.body.password to foundUser.password using the compare function
+        if (foundUser) {
+            isPasswordCorrect = await comparePasswords(request.body.password, foundUser.password);
+        } else {
+            return next(new Error("Incorrect username."));
+        }
 
-		response.json({
-			jwt: newJwt
-		});
-	} else {
-		return next(new Error("Incorrect password."));
-	}
-	} catch (error){
-		return next(error);
-	}
+        // Create a JWT based on foundUser._id
+        if (isPasswordCorrect) {
+            newJwt = createJWT(foundUser._id);
 
-})
+            response.json({
+                jwt: newJwt
+            });
+        } else {
+            return next(new Error("Incorrect password."));
+        }
+    } catch (error) {
+        return next(error);
+    }
+});
 
 module.exports = router;
