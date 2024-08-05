0c19e3ace6afceff6889ced16f622663388209d9
Taliesin <taliesinstudy@gmail.com>
2024-08-05 19:28:26 +1000

    feat! created events and made into key/value pairs

--- src/utils/trailHelper.js
+++ src/utils/trailHelper.js
@@ -1,31 +1,21 @@
 const { mongoose } = require("mongoose");
 const PokemonModel = require("../models/PokemonModel");
+const { events} = require("./events");
+
+const eventList = events;
 
 async function simulateTrail(trail, pokemonID) {
-    const events = [
-        `Pokemon found egg voucher`,
-        "Found a berry",
-        "Battled a wild Pokémon",
-        "Found a hidden item",
-        "Rested at a campsite",
-        "Crossed a river",
-        "Climbed a mountain",
-        "Encountered a trainer",
-        "Discovered a cave",
-        "Got lost and found the way back",
-        "Had a close call with a wild Pokémon"
-    ];
-
     // Find the specific ID of Pokemon on the trail
     const pokemonId =  trail.onTrail.find(pokemon => pokemon._id.toString() === pokemonID);
     const pokemon = await PokemonModel.findById(pokemonId);
     // Simulate the trail and create an event log
+    const eventKeys = Object.keys(eventList);
     const eventLog = [];
     const numberOfEvents = Math.floor(Math.random() * 5) + 1; // Each Pokémon will encounter between 1 and 5 events
 
     for (let i = 0; i < numberOfEvents; i++) {
-        const randomEvent = events[Math.floor(Math.random() * events.length)];
-        eventLog.push(randomEvent);
+        const randomEventKey = eventKeys[Math.floor(Math.random() * eventKeys.length)];
+        eventLog.push(randomEventKey);
     }
 
     console.log("HELPER " + pokemon.nickname);
@@ -36,7 +26,7 @@ async function simulateTrail(trail, pokemonID) {
 
     return {
         pokemonID: pokemonId,
-        trailLog: eventLog,
+        trailLog: eventLog,        
     };
 }
 
