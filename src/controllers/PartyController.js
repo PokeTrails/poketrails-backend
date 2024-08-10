f249dd39085af3f9079f01702f0e1df3f1ec2518
Suraj-Razor <14635@coderacademy.edu.au>
2024-08-10 18:31:45 +1000

    fix: added error handling to party controller

--- src/controllers/PartyController.js
+++ src/controllers/PartyController.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const { PartyModel } = require("../models/PartyModel");
 
-const getAllParties = async (req, res) => {
-    const parties = await PartyModel.findOne({ user: req.userId });
-    if (!parties) {
-        return res.status(404).json({
-            error: "user does not have a party"
+const getAllParties = async (req, res, next) => {
+    try {
+        const parties = await PartyModel.findOne({ user: req.userId });
+        if (!parties) {
+            return res.status(404).json({
+                error: "user does not have a party"
+            });
+        }
+        return res.status(200).json({
+            slots: parties.slots,
+            buffs: parties.buffs
         });
+    } catch (error) {
+        next(error);
     }
-    return res.status(200).json({
-        slots: parties.slots,
-        buffs: parties.buffs
-    });
 };
 
 module.exports = { getAllParties };
